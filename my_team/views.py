3e4ab15b9057e802a063c109247af59aaffa0866
kimjimin1101 <abcpanda@likelion.org>
2021-05-28 13:52:20 +0900

    likelion

--- my_team/views.py
+++ my_team/views.py
@@ -8,6 +8,6 @@ def hello(request):
 
     wordList = sentence.split(',')
 
-
-    return render(request, "hello.html",{'sentence':sentence, 'count':len(wordList),'sentence1': sentence.split(',')[0],'sentence2':sentence.split(',')[1],'sentence3':sentence.split(',')[2],'sentence4':sentence.split(',')[3]})
+    
+    return render(request, "hello.html",{'sentence':sentence, 'count':len(wordList),'wordList': wordList})
 
